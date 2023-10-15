6abdb02792cdfab9d2e7d06a67a6e263dcadd90e
24Fendi <146203928+24Fendi@users.noreply.github.com>
2023-10-15 19:28:34 +0700

    Update Library.sol

--- contracts/Library.sol
+++ contracts/Library.sol
@@ -3,19 +3,19 @@ pragma solidity ^0.8.13;
 
 contract Library {
     struct Book {
-        address ISBN; // Perubahan tipe data ISBN menjadi address
+        address ISBN; // Tipe data ISBN diganti menjadi address
         string title;
         uint256 year;
         string author;
     }
-    mapping (address =>Book) public bookDetail;
+    mapping (address => Book) public bookDetail;
 
     Book[] private books;
     address public admin;
 
     struct Log {
         string action;
-        address ISBN; // Juga perubahan tipe data ISBN menjadi address
+        address ISBN; // Tipe data ISBN diganti menjadi address
         bool success;
     }
 
@@ -31,7 +31,7 @@ contract Library {
     }
 
     function addBook(
-        address _ISBN, // Perubahan parameter _ISBN menjadi address
+        address _ISBN, // Tipe data parameter _ISBN diganti menjadi address
         string calldata _title,
         uint256 _year,
         string calldata _author
@@ -48,39 +48,39 @@ contract Library {
     }
 
     function updateBook(
-        address _ISBN, // Perubahan parameter _ISBN menjadi address
-        string calldata _title,
-        uint256 _year,
-        string calldata _author
+    address _ISBN, // Tipe data parameter _ISBN diganti menjadi address
+    string calldata _title,
+    uint256 _year,
+    string calldata _author
     ) public onlyAdmin {
-        require(_ISBN != address(0), "ISBN cannot be empty");
-        require(bytes(_title).length > 0, "Title cannot be empty");
-        require(_year > 0, "Year must be greater than 0");
-        require(bytes(_author).length > 0, "Author cannot be empty");
+    require(_ISBN != address(0), "ISBN cannot be empty");
+    require(bytes(_title).length > 0, "Title cannot be empty");
+    require(_year > 0, "Year must be greater than 0");
+    require(bytes(_author).length > 0, "Author cannot be empty");
 
-        // Cari buku dengan ISBN yang sesuai dalam array
-        for (uint256 i = 0; i < books.length; i++) {
-            if (books[i].ISBN == _ISBN) { // Perubahan cara membandingkan ISBN
-                // Temukan buku yang sesuai, lalu perbarui detailnya
-                books[i] = Book(_ISBN, _title, _year, _author);
+    // Cari buku dengan ISBN yang sesuai dalam array
+    for (uint256 i = 0; i < books.length; i++) {
+        if (books[i].ISBN == _ISBN) {
+            // Temukan buku yang sesuai, lalu perbarui detailnya
+            books[i] = Book(_ISBN, _title, _year, _author);
 
-                // Tambahkan log operasi
-                logs.push(Log("Update Book", _ISBN, true));
-                return;
-            }
+            // Tambahkan log operasi
+            logs.push(Log("Update Book", _ISBN, true));
+            return;
         }
-
-        // Jika buku tidak ditemukan
-        logs.push(Log("Update Book", _ISBN, false));
-        revert("Book with this ISBN does not exist");
     }
 
-    function removeBook(address _ISBN) public onlyAdmin { // Perubahan parameter _ISBN menjadi address
+    // Jika buku tidak ditemukan
+    logs.push(Log("Update Book", _ISBN, false));
+    
+}
+
+    function removeBook(address _ISBN) public onlyAdmin {
         require(_ISBN != address(0), "ISBN cannot be empty");
 
         // Cari buku dengan ISBN yang sesuai dalam array
         for (uint256 i = 0; i < books.length; i++) {
-            if (books[i].ISBN == _ISBN) { // Perubahan cara membandingkan ISBN
+            if (books[i].ISBN == _ISBN) {
                 // Temukan buku yang sesuai, lalu hapus
                 if (i < books.length - 1) {
                     books[i] = books[books.length - 1];
@@ -95,16 +95,16 @@ contract Library {
 
         // Jika buku tidak ditemukan
         logs.push(Log("Remove Book", _ISBN, false));
-        revert("Book with this ISBN does not exist");
+       
     }
 
-    function getBookByISBN(address _ISBN) // Perubahan parameter _ISBN menjadi address
+    function getBookByISBN(address _ISBN)
         public
         view
         returns (address, string memory, uint256, string memory)
     {
         for (uint256 i = 0; i < books.length; i++) {
-            if (books[i].ISBN == _ISBN) { // Perubahan cara membandingkan ISBN
+            if (books[i].ISBN == _ISBN) {
                 Book memory book = books[i];
                 return (book.ISBN, book.title, book.year, book.author);
             }
